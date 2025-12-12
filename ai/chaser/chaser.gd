06704e990a2ba88a8940992b4286c47e41b9df62
Sean <89282852+SodumbX@users.noreply.github.com>
2025-12-12 02:11:08 -0800

    Check for Walls

--- ai/chaser/chaser.gd
+++ ai/chaser/chaser.gd
@@ -2,6 +2,13 @@ extends Node2D
 
 @export_group("Nodes")
 @export var ChaserBody : RigidBody2D
+@export var WallCast : RayCast2D
+
+#Wall Detecting Raycast
+var checking_for_walls := false
+var wall_check_gaps := 18
+
+var wall_positions := []
 
 var recent_direction := Vector2.ZERO
 var recent_jump := false
@@ -9,7 +16,7 @@ var recent_jump := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 func send_input(direction : Vector2, jump : bool):
 	ChaserBody.input_left = direction.x
@@ -18,3 +25,20 @@ func send_input(direction : Vector2, jump : bool):
 	
 	recent_direction = direction
 	recent_jump = jump
+
+func check_for_walls() -> Array:
+	wall_positions = []
+	WallCast.global_position = ChaserBody.global_position
+	
+	if WallCast.rotation_degrees >= 360:
+		WallCast.rotation_degrees = 0
+		
+	while WallCast.rotation_degrees < 360:
+		WallCast.rotation_degrees += wall_check_gaps
+		
+		await get_tree().process_frame
+		
+		if WallCast.is_colliding():
+			wall_positions.append(WallCast.get_collision_point())
+		
+	return wall_positions
